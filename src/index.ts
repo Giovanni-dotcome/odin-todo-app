5cb55171b57234f3296fc2601328fbeb3f16ad66
Giovanni <giodevito99@gmail.com>
2024-12-30 21:53:28 +0100

    set up basic ui structure

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import Priority from "./components/Priority";
 import Project from "./components/Project";
 import TagsList from "./components/TagsList";
 
-const defaultProject = Project('default');
+const defaultProject = Project('Work stuff');
 
 const todoCleanBathroom = Todo('clean bathroom', 'needs to wash the floor and the wc', Priority.LOW, defaultProject, TagsList.filter(tag => tag.name === 'Personal' || tag.name === 'Maintenance'));
 
@@ -14,4 +14,17 @@ const projectHouseChores = Project('House Chores');
 todoCleanBathroom.setProject(projectHouseChores);
 projectHouseChores.deleteTodo(todoCleanBathroom.getId());
 
-console.log(projectHouseChores.getTodos());
+//console.log(projectHouseChores.getTodos());
+console.log('new test');
+
+const ul = document.querySelector('ul')
+const h1 = document.querySelector('h1')
+const main = document.querySelector('main')
+const mainUl = main?.querySelector('ul')
+
+if (ul)
+  ul.innerHTML = `<li>${defaultProject.getTitle()}</li>`;
+if (h1)
+  h1.innerHTML = `${defaultProject.getTitle()}`;
+if (mainUl)
+  mainUl.innerHTML = `${todoCleanBathroom.getTitle()}`;
